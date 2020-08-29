36739cf4529f86930a981bcab99b2b5db590f38c
mkhoa <mkhoa94@gmail.com>
2020-08-30 04:15:47 +0700

    Optimize Performance

--- app.py
+++ app.py
@@ -1,9 +1,60 @@
 # -*- coding: utf-8 -*-
+import streamlit as st
 import pickle
 
-# Load saved model
-clf = pickle.load(open('model.sav', 'rb'))
+# Streamlit encourages well-structured code, like starting execution in a main() function.
+def main():
+    # Once we have the dependencies, add a selector for the app mode on the sidebar.
+    st.title('Movie Review Sentiment Analysis')
+    st.sidebar.title("What to do")
+    app_mode = st.sidebar.selectbox("Choose the app mode",
+        ["Show instructions", "Run the app", "Show the source code"])
+    if app_mode == "Show instructions":
+        st.sidebar.success('To continue select "Run the app".')
+    elif app_mode == "Show the source code":
+        st.write('Soure Code: https://github.com/mkhoa/rottentomatoes')
+    elif app_mode == "Run the app":
+        run_the_app()
 
-#predict function
-def predict(text):
-    return clf.predict([text])
+# Load saved model
+@st.cache(allow_output_mutation=True)
+def load_model():
+    model1 = pickle.load(open('model1.sav', 'rb'))
+    model2 = pickle.load(open('model2.sav', 'rb'))
+    return model1, model2
+
+# Predict function by model 1, non-convertred sentiment
+def model1_predict(text):
+    prediction = model1.predict([text])
+    if prediction==0:  
+        return 'Negative'
+    elif prediction==1:
+        return 'Somewhat negative'
+    elif prediction==2:
+        return 'Neutral'
+    elif prediction==3:
+        return 'Somewhat positive'
+    elif prediction==4:
+        return 'Positive'
+    
+# Predict function
+def model2_predict(text):
+    prediction = model2.predict([text])
+    if prediction==0:  
+        return 'Negative'
+    elif prediction==1:
+        return 'Neutral'
+    elif prediction==2:
+        return 'Positive'
+
+# Load saved model
+model1, model2 = load_model()
+def run_the_app():
+    user_input = st.text_input("Review Text", 'This is an example review for movie')
+    st.write('Prediction by model 1, training by using original sentiment')
+    st.write(model1_predict(user_input))
+    st.write('Prediction by model 2, training by using converted sentiment')
+    st.write(model2_predict(user_input))
+    
+if __name__ == "__main__":
+    main()
--- training.py
+++ training.py
@@ -21,13 +21,6 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 
 
-# Load data
-df = pd.read_csv('train.csv', index_col='Unnamed: 0')
-
-# Download stopwords
-# nltk.download('stopwords')
-stop_words = stopwords.words('english')
-
 #Function to clean data
 def preprocessor(text):
     """ Return a cleaned version of text
@@ -63,61 +56,75 @@ def convert(sentiment):
         sentiment = 2 # Positive
     
     return sentiment
+
+def main():
+    """Function to run training model
     
-# Convert
-df['converted_sentiment'] = df['Sentiment'].apply(convert)
-df['preprocessed'] = df['Phrase'].apply(preprocessor)
-df['preprocessed'] = df['preprocessed'].apply(tokenizer_porter)
-
-y1 = df['Sentiment']
-y2 = df['converted_sentiment']
-X = df['Phrase']
-
-# Training model on non-converted Sentiment
-X_train, X_test, y_train, y_test = train_test_split(X, y1, test_size=0.2, random_state=0)
-# tfid = TfidfVectorizer(stop_words=stop_words, tokenizer=tokenizer_porter, preprocessor=preprocessor)
-count = CountVectorizer(stop_words=stop_words, tokenizer=tokenizer_porter, preprocessor=preprocessor)
-
-# Construct pipeline
-clf = Pipeline([('vect', count), ('clf', LogisticRegression(random_state=0, max_iter=400))])
-clf.fit(X_train, y_train)
-prediction = clf.predict(X_test)
-
-#Print Result
-print('accuracy:',accuracy_score(y_test,prediction))
-print('confusion matrix:\n',confusion_matrix(y_test,prediction))
-print('classification report:\n',classification_report(y_test,prediction))
-
-# df_test = pd.DataFrame({'Text': X_test, 'Actual': y_test, 'Predict': prediction})
-# df_test = df_test[df_test['Actual'] != df_test['Predict']]
-# df_test['processed'] = df_test['Text'].apply(preprocessor)
-# df_test['processed'] = df_test['processed'].apply(tokenizer_porter)
-
-# Save trained model to disk
-pickle.dump(clf, open('model1.sav', 'wb')) 
-
-# Train model on converted sentiment
-X_train, X_test, y_train, y_test = train_test_split(X, y2, test_size=0.2, random_state=0)
-# tfid = TfidfVectorizer(stop_words=stop_words, tokenizer=tokenizer_porter, preprocessor=preprocessor)
-count = CountVectorizer(stop_words=stop_words, tokenizer=tokenizer_porter, preprocessor=preprocessor)
-
-# Construct pipeline
-clf = Pipeline([('vect', count), ('clf', LogisticRegression(random_state=0, max_iter=400))])
-clf.fit(X_train, y_train)
-prediction = clf.predict(X_test)
-
-#Print Result
-print('accuracy:',accuracy_score(y_test,prediction))
-print('confusion matrix:\n',confusion_matrix(y_test,prediction))
-print('classification report:\n',classification_report(y_test,prediction))
-
-# df_test = pd.DataFrame({'Text': X_test, 'Actual': y_test, 'Predict': prediction})
-# df_test = df_test[df_test['Actual'] != df_test['Predict']]
-# df_test['processed'] = df_test['Text'].apply(preprocessor)
-# df_test['processed'] = df_test['processed'].apply(tokenizer_porter)
-
-# Save trained model to disk
-pickle.dump(clf, open('model2.sav', 'wb')) 
+    """
+    # Load data
+    df = pd.read_csv('train.csv', index_col='Unnamed: 0')
+    
+    # Download stopwords
+    nltk.download('stopwords')
+    stop_words = stopwords.words('english')
+        
+    # Convert
+    df['converted_sentiment'] = df['Sentiment'].apply(convert)
+    df['preprocessed'] = df['Phrase'].apply(preprocessor)
+    df['preprocessed'] = df['preprocessed'].apply(tokenizer_porter)
+    
+    y1 = df['Sentiment']
+    y2 = df['converted_sentiment']
+    X = df['Phrase']
+    
+    # Training model on non-converted Sentiment
+    X_train, X_test, y_train, y_test = train_test_split(X, y1, test_size=0.2, random_state=0)
+    # tfid = TfidfVectorizer(stop_words=stop_words, tokenizer=tokenizer_porter, preprocessor=preprocessor)
+    count = CountVectorizer(stop_words=stop_words, tokenizer=tokenizer_porter, preprocessor=preprocessor)
+    
+    # Construct pipeline
+    clf = Pipeline([('vect', count), ('clf', LogisticRegression(random_state=0, max_iter=400))])
+    clf.fit(X_train, y_train)
+    prediction = clf.predict(X_test)
+    
+    #Print Result
+    print('accuracy:',accuracy_score(y_test,prediction))
+    print('confusion matrix:\n',confusion_matrix(y_test,prediction))
+    print('classification report:\n',classification_report(y_test,prediction))
+    
+    # df_test = pd.DataFrame({'Text': X_test, 'Actual': y_test, 'Predict': prediction})
+    # df_test = df_test[df_test['Actual'] != df_test['Predict']]
+    # df_test['processed'] = df_test['Text'].apply(preprocessor)
+    # df_test['processed'] = df_test['processed'].apply(tokenizer_porter)
+    
+    # Save trained model to disk
+    pickle.dump(clf, open('model1.sav', 'wb')) 
+    
+    # Train model on converted sentiment
+    X_train, X_test, y_train, y_test = train_test_split(X, y2, test_size=0.2, random_state=0)
+    # tfid = TfidfVectorizer(stop_words=stop_words, tokenizer=tokenizer_porter, preprocessor=preprocessor)
+    count = CountVectorizer(stop_words=stop_words, tokenizer=tokenizer_porter, preprocessor=preprocessor)
+    
+    # Construct pipeline
+    clf = Pipeline([('vect', count), ('clf', LogisticRegression(random_state=0, max_iter=400))])
+    clf.fit(X_train, y_train)
+    prediction = clf.predict(X_test)
+    
+    #Print Result
+    print('accuracy:',accuracy_score(y_test,prediction))
+    print('confusion matrix:\n',confusion_matrix(y_test,prediction))
+    print('classification report:\n',classification_report(y_test,prediction))
+    
+    # df_test = pd.DataFrame({'Text': X_test, 'Actual': y_test, 'Predict': prediction})
+    # df_test = df_test[df_test['Actual'] != df_test['Predict']]
+    # df_test['processed'] = df_test['Text'].apply(preprocessor)
+    # df_test['processed'] = df_test['processed'].apply(tokenizer_porter)
+    
+    # Save trained model to disk
+    pickle.dump(clf, open('model2.sav', 'wb')) 
+    
+if __name__ == '__main__':
+    main()
 
 
 
